7c05eaf63f66c5af980709c6decb3a7b95d772b4
jdanielgoh <48138542+jdanielgoh@users.noreply.github.com>
2025-06-05 11:01:25 -0600

    quitando un conflicto pendiente

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
 
   modules: [
-<<<<<<< HEAD
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/test-utils',
-    '@nuxt/image',
-    '@sidebase/nuxt-auth'
-  ]
-=======
-    '@pinia/nuxt',
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/test-utils'
+    "@pinia/nuxt",
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxt/test-utils",
   ],
 
-  css: [
-    '@centrogeomx/sisdai-css/dist/sisdai.min.css',
-  ],
->>>>>>> feature/nuxt3
-})
+  css: ["@centrogeomx/sisdai-css/dist/sisdai.min.css"],
+});
