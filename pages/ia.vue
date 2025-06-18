5c86aba9a9b044c741548f70c8571df76a5d3251
ciretorres <erictorres.velasco@gmail.com>
2025-06-17 18:08:58 -0600

    🔧 feat: agregando página ia

---
+++ pages/ia.vue
@@ -0,0 +1,27 @@
+<script setup></script>
+<template>
+  <main 
+    id="ia"
+  >
+    <h2>Módulo de IA</h2>
+    <p>
+      Esta herramienta está diseñada para ayudarte a analizar información científica y territorial mediante inteligencia artificial. Puedes crear proyectos temáticos, cargar datos relevantes, hacer preguntas específicas y generar reportes automatizados.
+    </p>
+    <section 
+      id="proyectos"
+      class="contenedor"
+    >
+      <h3>Mis proyectos</h3>
+      <button 
+        type="button"
+        class="boton-primario"
+      >
+        Crear nuevo proyecto
+        <span 
+          class="pictograma-agregar" 
+          aria-hidden="true"
+        ></span>
+      </button>
+    </section>
+  </main>
+</template>
