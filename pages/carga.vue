ae318c37cb94d3d09dfa546eb658f247db96c4d8
Jesús Daniel Gómez Hernández <psp.dgomez@centrogeo.edu.mx>
2025-06-19 11:35:37 -0600

    prueba

--- pages/carga.vue
+++ pages/carga.vue
@@ -1,10 +1,23 @@
-<script setup></script>
+<script setup>
+import SisdaiCampoBase from "@centrogeomx/sisdai-componentes/src/componentes/campo-base/SisdaiCampoBase.vue";
+import { ref, watch } from "vue";
+const archivo = ref("");
+watch(archivo, (nv, ov) => {
+  console.log(nv, ov);
+});
+</script>
 <template>
   <main>
     <h2>Esto es la vista de garga</h2>
     <section id="">
       <div class="contenedor">
         <h3>Sección</h3>
+        <SisdaiCampoBase
+          etiqueta="Campo de archivo"
+          ejemplo="tipo file"
+          tipo="file"
+          v-model="archivo"
+        />
       </div>
     </section>
   </main>
