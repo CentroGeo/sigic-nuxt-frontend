d0415030e9dad12aad3812d0a2717fbcea636d23
ciretorres <erictorres.velasco@gmail.com>
2025-06-24 12:28:13 -0600

    🧪 feat: agregando pruebas para chat ai

---
+++ pages/inteligencia-artificial/chat.vue
@@ -0,0 +1,100 @@
+<script setup>
+const proyecto = {
+  nombre: "Agua",
+  privado: true,
+};
+const listaContexto = [
+  {
+    nombre: "Contexto 1",
+    enlace: "#",
+    fuentes: "3 fuentes",
+  },
+  {
+    nombre: "Agua de la región sureste",
+    enlace: "/agua-de-la-region-sureste/",
+    fuentes: "10 fuentes",
+  },
+  {
+    nombre: "Contexto 3",
+    enlace: "#",
+    fuentes: "3 fuentes",
+  },
+  {
+    nombre: "Contexto 4",
+    enlace: "#",
+    fuentes: "10 fuentes",
+  },
+];
+</script>
+
+<template>
+  <main>
+    <div class="flex">
+      <div class="columna-4 columna-1-mov menu-lateral-fondo">
+        <nav class="menu-lateral">
+          <div class="menu-lateral-contenedor">
+            <NuxtLink to="/inteligencia-artificial">
+              <span class="pictograma-flecha-izquierda" aria-hidden="true" />
+              Regresar al proyecto
+            </NuxtLink>
+            <p>{{ proyecto.nombre }}</p>
+            <p v-if="proyecto.privado">
+              Privado <span class="pictograma-ayuda" aria-hidden="true" />
+            </p>
+            <ul>
+              <li v-for="contexto in listaContexto" :key="contexto.nombre">
+                <a
+                  :href="contexto.enlace"
+                  tabindex=""
+                  :class="{
+                    /*
+                'router-link-exact-active router-link-active': isActive(
+                  page.relativePath,
+                  item.link
+                ),*/
+                  }"
+                >
+                  <p>{{ contexto.nombre }}</p>
+                  <p class="fuentes">{{ contexto.fuentes }}</p>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+      <div class="contenedor columna-12 columna-7-mov">
+        <div>
+          <h1>Chat SIGIC</h1>
+          <h2>Proyecto seleccionado: {{ proyecto.nombre }}</h2>
+        </div>
+
+        <div>
+          <!-- showChatBar -->
+          <!-- showDateIncorrect -->
+          <!-- chatLog -->
+          <!-- showChatLoader -->
+        </div>
+
+        <div>
+          <form @submit.prevent>
+            <label for="area-idcreadoautomaticamente" class="a11y-solo-lectura">
+              Nombre de la etiqueta
+            </label>
+
+            <textarea
+              id="area-idcreadoautomaticamente"
+              placeholder="Envía un mensaje"
+            />
+            <button
+              class="boton-pictograma boton-con-contenedor-primario"
+              aria-label="Enviar mensaje"
+              type="button"
+            >
+              <span class="pictograma-ayuda" aria-hidden="true" />
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
---
+++ pages/inteligencia-artificial/index.vue
@@ -0,0 +1,71 @@
+<script setup></script>
+<template>
+  <main id="ia">
+    <h2>Módulo de IA</h2>
+    <p>
+      Esta herramienta está diseñada para ayudarte a analizar información
+      científica y territorial mediante inteligencia artificial. Puedes crear
+      proyectos temáticos, cargar datos relevantes, hacer preguntas específicas
+      y generar reportes automatizados.
+    </p>
+    <section id="proyectos">
+      <div class="contenedor">
+        <div class="flex flex-contenido-separado">
+          <h3>Mis proyectos</h3>
+          <button type="button" class="boton-primario">
+            Crear nuevo proyecto
+            <span class="pictograma-agregar" aria-hidden="true" />
+          </button>
+        </div>
+
+        <div class="flex">
+          <div class="columna-4">
+            <div class="tarjeta tarjeta-horizontal">
+              <div class="tarjeta-cuerpo">
+                <p class="tarjeta-etiqueta">
+                  <button
+                    class="boton-pictograma boton-sin-contenedor-primario"
+                    aria-label="Editar"
+                    type="button"
+                  >
+                    <span class="pictograma-editar" aria-hidden="true" />
+                  </button>
+                  <button
+                    class="boton-pictograma boton-sin-contenedor-primario"
+                    aria-label="Remover"
+                    type="button"
+                  >
+                    <span class="pictograma-eliminar" aria-hidden="true" />
+                  </button>
+                </p>
+                <p class="tarjeta-titulo">Agua de la región sureste</p>
+                <p>4 fuentes</p>
+              </div>
+              <div class="tarjeta-pie">
+                <NuxtLink
+                  class="boton-pictograma boton-sin-contenedor-primario"
+                  aria-label="Iniciar chat"
+                  to="inteligencia-artificial/chat"
+                >
+                  <span class="pictograma-ayuda" aria-hidden="true" />
+                  Iniciar chat
+                </NuxtLink>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<style>
+@media screen and (min-width: 768px) {
+  .tarjeta-horizontal .tarjeta-cuerpo {
+    padding-left: 24px;
+  }
+  .tarjeta-horizontal .tarjeta-pie {
+    padding-left: 24px;
+  }
+}
+</style>
