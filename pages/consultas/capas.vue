6ee9752d009e231bb8ebb52427427575d5aaf01d
andres-geotec <andres.geotec@gmail.com>
2025-07-01 12:51:18 -0600

    🐛 fix: consulta paneles en un solo archivo como slots

---
+++ pages/consultas/capas.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <ConsultaLayoutPaneles>
+    <template #catalogo>Capas disponibles</template>
+
+    <template #visualizador>Mapa</template>
+
+    <template #seleccion>Capas seleccionadas</template>
+  </ConsultaLayoutPaneles>
+</template>
