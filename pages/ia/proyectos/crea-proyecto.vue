75335532763f09b05f8a8096a5edd0d1f709863d
Jose Luis <juc@centrogeo.edu.mx>
2025-07-07 14:28:01 -0600

    Agregado formulario para crear proyecto

--- pages/ia/proyectos/crea-proyecto.vue
+++ pages/ia/proyectos/crea-proyecto.vue
@@ -1,6 +1,10 @@
 <script setup>
 import LayoutIA from "~/components/ia/LayoutIA.vue";
 import PanelListas from "~/components/ia/PanelListas.vue";
+import SisdaiCampoBase from "@centrogeomx/sisdai-componentes/src/componentes/campo-base/SisdaiCampoBase.vue";
+import SisdaiAreaTexto from "@centrogeomx/sisdai-componentes/src/componentes/area-texto/SisdaiAreaTexto.vue";
+import SisdaiGrupoBotonesRadio from "@centrogeomx/sisdai-componentes/src/componentes/boton-radio-grupo/SisdaiBotonesRadioGrupo.vue";
+import SisdaiBotonRadio from "@centrogeomx/sisdai-componentes/src/componentes/boton-radio/SisdaiBotonRadio.vue";
 
 import { useIAStore } from "~/stores/ia.js";
 const storeIA = useIAStore();
@@ -17,7 +21,61 @@ const storeIA = useIAStore();
       <div class="columna-12">
         <!-- Vista CreaProyecto -->
         <div class="contenedor">
-          <h2>Configuración del Proyecto</h2>
+          <h2>Configuración de proyecto</h2>
+          <div class="grid">
+            <div class="columna-10">
+              <form action="">
+                <SisdaiCampoBase
+                  etiqueta="Nombre del proyecto"
+                  ejemplo=""
+                  :es_etiqueta_visible="true"
+                  class="m-b-3"
+                />
+
+                <SisdaiAreaTexto
+                  etiqueta="Descripción del proyecto"
+                  class="m-b-3"
+                />
+
+                <SisdaiGrupoBotonesRadio leyenda="Visibilidad">
+                  <SisdaiBotonRadio
+                    etiqueta="Público"
+                    value="publico"
+                    name="visibilidad"
+                  />
+                  <SisdaiBotonRadio
+                    etiqueta="Privado"
+                    value="privado"
+                    name="visibilidad"
+                  />
+                </SisdaiGrupoBotonesRadio>
+              </form>
+            </div>
+          </div>
+          <div class="grid">
+            <div class="columna-16">
+              <div class="separador m-b-3"></div>
+              <div class="flex flex-contenido-separado fuentes-encabezado">
+                <h2>Agregar fuentes de información</h2>
+                <div class="">
+                  <button
+                    class="boton-pictograma boton-primario m-r-2"
+                    aria-label="Agregar del catalogo"
+                  >
+                    Agregar del catálogo
+                    <span class="pictograma-agregar" aria-hidden="true" />
+                  </button>
+                  <button
+                    class="boton-pictograma boton-primario"
+                    aria-label="Subir archivos"
+                  >
+                    Subir archivos
+                    <span class="pictograma-archivo-subir" aria-hidden="true" />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
           <NuxtLink
             class="boton boton-primario"
             aria-label="Crear proyecto"
@@ -31,3 +89,15 @@ const storeIA = useIAStore();
     </div>
   </LayoutIA>
 </template>
+
+<style lang="scss">
+.separador {
+  width: 100%;
+  height: 1px;
+  background: #aaa;
+}
+
+.fuentes-encabezado {
+  align-items: center;
+}
+</style>
