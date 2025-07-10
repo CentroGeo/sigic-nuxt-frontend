0e1237faaa6b974ab253098c774dfa8ef5e774bf
ciretorres <erictorres.velasco@gmail.com>
2025-07-10 11:12:30 -0600

    🛠 refactor: organizando componentes para layout, listas y vistas

---
+++ components/ia/NavegacionLateral.vue
@@ -0,0 +1,69 @@
+<!-- TODO: esta se reúne con
+la de ~/components/consulta/NavegacionLateral.vue -->
+<script setup>
+const ruta = "/ia";
+const subPaginas = [
+  {
+    picto: "pictograma-ayuda",
+    ruta: `${ruta}/chats`,
+  },
+  {
+    picto: "pictograma-ayuda",
+    ruta: `${ruta}/proyectos`,
+  },
+];
+</script>
+
+<template>
+  <div class="nav-lateral" aria-label="Navegación Lateral">
+    <ul class="lista-sin-estilo">
+      <li>
+        <div class="avatar-imagen">
+          <img
+            src="https://cdn.conahcyt.mx/sisdai/sisdai-css/documentacion/nilo.jpg"
+            alt=""
+          />
+        </div>
+      </li>
+
+      <li v-for="subPagina in subPaginas" :key="`elemento-${subPagina.ruta}`">
+        <nuxt-link
+          class="boton-pictograma boton-sin-contenedor-primario"
+          :to="subPagina.ruta"
+        >
+          <span :class="subPagina.picto" aria-hidden="true" />
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.nav-lateral {
+  width: 48px;
+  box-shadow: 4px 0 8px -4px var(--navegacion-primaria-sombra);
+  z-index: 2;
+
+  ul {
+    padding: 0 8px;
+    // list-style: none;
+
+    li {
+      .avatar-imagen {
+        border-radius: 32px;
+        height: 32px;
+        width: 32px;
+        overflow: hidden;
+        img {
+          object-fit: cover;
+        }
+      }
+
+      .router-link-exact-active {
+        background: var(--boton-sin-contenedor-primario-enfoque-fondo);
+        color: var(--boton-sin-contenedor-primario-enfoque-color);
+      }
+    }
+  }
+}
+</style>
---
+++ components/ia/vista/LeyendaInicio.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="contenedor ancho-lectura">
+      <div class="flex">
+        <div class="columna-16 flex-vertical-centrado" style="height: 85vh">
+          <h2 class="texto-centrado">
+            Analiza información con Inteligencia Artificial
+          </h2>
+          <div class="fondo-color-acento p-2 borde-redondeado-8">
+            <h6 class="m-t-0">Crea un proyecto para comenzar</h6>
+            <p>
+              Esta herramienta está diseñada para ayudarte a analizar
+              información científica y territorial mediante chats con
+              inteligencia artificial. Para iniciar, crea proyectos temáticos,
+              agregar información del catálogo o subir tus propios archivos.
+            </p>
+          </div>
+          <div class="texto-centrado m-t-3">
+            <NuxtLink
+              class="boton-pictograma boton-primario"
+              aria-label="Crear nuevo proyecto"
+              to="/ia/proyecto/crea-proyecto"
+            >
+              Crear proyecto
+              <span class="pictograma-agregar" aria-hidden="true" />
+            </NuxtLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
