dc2f9354179c21b45e8837ed9ae2c8efa983d450
Jesús Daniel Gómez Hernández <psp.dgomez@centrogeo.edu.mx>
2025-07-17 16:10:09 -0600

    Adecuando estilos para menu de gobierno

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,6 +10,7 @@ const accesibilidadStore = useAccesibilidadStore();
     <a href="#principal" class="ir-contenido-principal">
       Ir a contenido principal
     </a>
+
     <MainNavegacion />
 
     <main id="principal" class="p-t-7">
@@ -27,7 +28,34 @@ const accesibilidadStore = useAccesibilidadStore();
 nav.navegacion.navegacion-pegada {
   top: 60px;
 }
-.navbar-brand {
-  margin-right: 41rem;
+:root {
+  --escala-rem-gob-sisdai: 0.6428571429;
+}
+nav.navbar {
+  padding-top: calc(var(--escala-rem-gob-sisdai) * 0.5rem);
+  padding-bottom: calc(var(--escala-rem-gob-sisdai) * 0.5rem);
+}
+a.navbar-brand {
+  margin-right: calc(var(--escala-rem-gob-sisdai) * 63rem);
+  margin-top: calc(var(--escala-rem-gob-sisdai) * 0.3125rem);
+  margin-bottom: calc(var(--escala-rem-gob-sisdai) * 0.3125rem);
+}
+.navbar-expand-md .navbar-nav .nav-link {
+  padding-right: calc(var(--escala-rem-gob-sisdai) * 0.5rem);
+  padding-left: calc(var(--escala-rem-gob-sisdai) * 1.5rem);
+  padding-top: calc(var(--escala-rem-gob-sisdai) * 1.8rem);
+}
+@media (min-width: 576px) and (max-width: 775px) {
+  .navbar-dark .navbar-toggler {
+    margin-right: calc(var(--escala-rem-gob-sisdai) * 9rem);
+  }
+  a.navbar-brand {
+    margin-right: calc(var(--escala-rem-gob-sisdai) * 41rem);
+  }
+}
+.navbar-toggler-icon {
+  display: inline-block;
+  width: calc(var(--escala-rem-gob-sisdai) * 1.5em);
+  height: calc(var(--escala-rem-gob-sisdai) * 1.5em);
 }
 </style>
