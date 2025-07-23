5de2653e3d90619b0c953af2b29e8d75f170b5e7
ciretorres <erictorres.velasco@gmail.com>
2025-07-22 21:31:52 -0600

    🔧 feat: api para leer múltiples archivos y almacenar en storage de nitro

---
+++ server/api/subirV2.post.ts
@@ -0,0 +1,18 @@
+export default defineEventHandler(async (event) => {
+  try {
+    const files = await readMultipartFormData(event)
+    // imprime los archivos del lado del servidor
+    console.log(files)
+    if (!files) {
+      return createError({ statusCode: 400, statusMessage: 'Los archivos están vacíos' })
+    }
+    // TODO: subir files al backend
+    for (const file of files) {
+      // TODO: remover useStorage cuando se conecte al backend
+      await useStorage().setItemRaw(`fs:${file.filename}`, file.data)
+    }
+    return 200
+  } catch (error) {
+    return createError({ statusCode: 500, statusMessage: 'Algo salió mal con tu api' })
+  }
+})
