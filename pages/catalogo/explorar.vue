ff208c7ca973d81b79164382e3667d93133295a7
ciretorres <erictorres.velasco@gmail.com>
2025-07-28 09:44:20 -0600

    se me olvidó la lista filtrada en los filtros avanzados

--- pages/catalogo/explorar.vue
+++ pages/catalogo/explorar.vue
@@ -2,6 +2,37 @@
 // TODO: demo recursos
 // const { listaRecursos } = useDemoGeonodeResources();
 // console.log("listaRecursos", listaRecursos);
+const recursosFiltrados = ref([
+  { id: "bash", nombre: "Bash" },
+  { id: "c", nombre: "C" },
+  { id: "cpp", nombre: "C++" },
+  { id: "csharp", nombre: "C#" },
+  { id: "css", nombre: "CSS" },
+  { id: "dart", nombre: "Dart" },
+  { id: "docker", nombre: "Docker" },
+  { id: "elixir", nombre: "Elixir" },
+  { id: "go", nombre: "Go" },
+  { id: "graphql", nombre: "GraphQL" },
+  { id: "html", nombre: "HTML" },
+  { id: "java", nombre: "Java" },
+  { id: "javascript", nombre: "JavaScript" },
+  { id: "json", nombre: "JSON" },
+  { id: "kotlin", nombre: "Kotlin" },
+  { id: "markdown", nombre: "Markdown" },
+  { id: "php", nombre: "PHP" },
+  { id: "python", nombre: "Python" },
+  { id: "r", nombre: "R" },
+  { id: "ruby", nombre: "Ruby" },
+  { id: "rust", nombre: "Rust" },
+  { id: "scss", nombre: "SCSS" },
+  { id: "shell", nombre: "Shell" },
+  { id: "sql", nombre: "SQL" },
+  { id: "swift", nombre: "Swift" },
+  { id: "typescript", nombre: "TypeScript" },
+  { id: "xml", nombre: "XML" },
+  { id: "yaml", nombre: "YAML" },
+]);
+const recursosTipo = ref("dataLayer");
 </script>
 
 <template>
