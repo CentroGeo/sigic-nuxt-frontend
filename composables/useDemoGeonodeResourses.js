977edc0ddf893bdbb3dac171ddb415c8174c1e37
ciretorres <erictorres.velasco@gmail.com>
2025-07-28 23:37:03 -0600

    🛠 refactor: se agrega fetch a composable regresa listaRecursos y data completa

--- composables/useDemoGeonodeResourses.js
+++ composables/useDemoGeonodeResourses.js
@@ -1,6 +1,7 @@
 // https://stable.demo.geonode.org/api/v2/resources?page=1&page_size=15&filter%7Bresource_type%7D=dataset
 console.log("hola");
 // const listaRecursos = ref([]);
+const listaRecursosV2 = ref([]);
 export function useDemoGeonodeResources() {
   // console.log("adiós");
   // fetch(
@@ -13,18 +14,18 @@ export function useDemoGeonodeResources() {
   //   listaRecursos.value = response;
   // });
 
-  const listaRecursos = ref(
-    fetch(
-      "https://stable.demo.geonode.org/api/v2/resources?page=1&page_size=15&filter%7Bresource_type%7D=dataset",
-      {
-        method: "GET",
-      }
-    ).then((response) => {
-      console.log("response", response);
-      // return response;
-      if (response.ok) return response.json();
-    })
-  );
+  // const listaRecursos = ref(
+  //   fetch(
+  //     "https://stable.demo.geonode.org/api/v2/resources?page=1&page_size=15&filter%7Bresource_type%7D=dataset",
+  //     {
+  //       method: "GET",
+  //     }
+  //   ).then((response) => {
+  //     console.log("response", response);
+  //     // return response;
+  //     if (response.ok) return response.json();
+  //   })
+  // );
 
   // const listaRecursos = async () => {
   //   ref(
@@ -47,7 +48,54 @@ export function useDemoGeonodeResources() {
   //   );
   // };
 
+  const data = fetch(
+    "https://stable.demo.geonode.org/api/v2/resources?page=1&page_size=15&filter%7Bresource_type%7D=dataset",
+    {
+      method: "GET",
+    }
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      // TODO: aquí se pueden obtener las distintas propiedades
+      // links, page, page_size, resorces, total
+      const dataJSON = json;
+      // enlaces.value = json.links;
+      // pagina.page = value.page;
+      // paginaTamanio.value = json.page_size;
+      // TODO: iterar sobre las propiedades necesarias
+      // listaRecursosV2.value = json.resources;
+      json.resources.forEach((resource) => {
+        listaRecursosV2.value.push({
+          // pk: resource.pk,
+          // uuid: resource.uuid,
+          // title: resource.title,
+          resource_type: resource.resource_type,
+          // keywords: resource.keywords,
+          // category: resource.category,
+          // abstract: resource.abstract,
+          // attribution: resource.attribution,
+          // thumbnail_url: resource.thumbnail_url,
+          // extent: resource.extent,
+          // date: resource.date,
+          // date_type: resource.date_type,
+          // created: resource.created,
+          last_updated: resource.last_updated,
+        });
+        // total.value = json.total;
+      });
+      return dataJSON;
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+    .finally(() => {
+      //console.log("fin");
+    });
+
   return {
-    listaRecursos,
+    listaRecursosV2,
+    data,
   };
 }
--- server/api/subirV2.post.ts
+++ server/api/subirV2.post.ts
@@ -6,11 +6,42 @@ export default defineEventHandler(async (event) => {
     if (!files) {
       return createError({ statusCode: 400, statusMessage: 'Los archivos están vacíos' })
     }
-    // TODO: subir files sld al backend
+
     for (const file of files) {
       // TODO: remover useStorage cuando se conecte al backend
       await useStorage().setItemRaw(`fs:${file.filename}`, file.data)
     }
+
+    // TODO: subir files sld al backend
+    //// const res = await fetch(`${configEnv.public.geonodeApi}/uploads/upload/`, {
+    // const response = await fetch("http://10.2.102.99/upload/uploads/upload", {
+    //   headers: {
+    //     "Authorization": `Bearer ${data.fields.token[0]}`,
+    //     "accept": "*/*",
+    //     "accept-language": "en-US,en;q=0.9",
+    //     "content-type": "multipart/form-data; boundary=----WebkitFormBoundaryb6Ed1YzAuqRpzbdL",
+    //     "x-csrftoken": "7FPk21mZjOhvU63jeaRt0TSDvscOL0kv",
+    //     "x-requested-with": "XMLHttpRequest"
+    //   },
+    //   referrer: "http://10.2.102.99/datasets/geonode:coordinaciones/style_upload",
+    //   body: "filename=\"simple_green-point.sld\"",
+    //   method: "POST",
+    //   mode: "cors",
+    //   credentials: "include"
+    // })
+    // console.log("response.status:", response.status);
+    // if (!response.ok) {
+    //   throw new Error(`Error ${response.status}:`)
+    // }
+    // const { dataset } = await response.json();
+    // const json = await response.json();
+    // console.log("json:", json);
+    // // return json;
+    // return {
+    //   html: dataset.raw_abstract,
+    //   text: dataset.abstract
+    // }
+
     return 200
   } catch (error) {
     return createError({ statusCode: 500, statusMessage: 'Algo salió mal con tu api' })
