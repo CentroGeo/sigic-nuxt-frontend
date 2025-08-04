c21df0366c5c1ce2ad034caae981bf0829184326
Jesús Daniel Gómez Hernández <psp.dgomez@centrogeo.edu.mx>
2025-08-04 10:48:41 -0600

    Avance en desarrollo de vista de inicio

--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,275 @@ definePageMeta({ auth: false });
 </script>
 <template>
   <div>
-    <h2>pages/index</h2>
-    <section id="">
-      <div class="contenedor">
-        <h3>Sección</h3>
+    <main id="principal">
+      <div class="portada portada-secundaria">
+        <img
+          class="portada-imagen"
+          src="https://cdn.conahcyt.mx/sisdai/sisdai-css/documentacion/godo.jpg"
+          alt=""
+        />
+
+        <div class="portada-degradado">
+          <div class="portada-cuerpo">
+            <h1 class="portada-titulo">
+              Sistema Integral de Gestión de Información Científica (SIGIC)
+            </h1>
+            <strong class="portada-subtitulo">
+              Integra, visualiza y aprovecha el conocimiento científico de México
+            </strong>
+          </div>
+        </div>
       </div>
-    </section>
+
+      <section id="que-es" class="m-y-10">
+        <div class="contenedor ancho-lectura texto-centrado m-b-10">
+          <h2>¿Qué es SIGIC?</h2>
+          <p>
+            SIGIC permite articular información estructurada de las ciencias, humanidades,
+            tecnologías e innovación en un ecosistema digital que impulsa la toma de decisiones, el
+            entendimiento territorial y la generación de nuevo conocimiento.
+          </p>
+          <p>
+            Con módulos para visualizar capas, generar mapas, cargar información propia y aprovechar
+            inteligencia artificial, SIGIC ofrece herramientas clave para investigar, planear y
+            actuar con base en datos.
+          </p>
+        </div>
+        <div class="contenedor ancho-fijo">
+          <div class="flex">
+            <div class="columna-8">
+              <div class="tarjeta tarjeta-horizontal">
+                <img
+                  class="tarjeta-imagen"
+                  src="https://cdn.conahcyt.mx/sisdai/sisdai-css/documentacion/pekibb.jpg"
+                />
+                <div class="tarjeta-cuerpo">
+                  <p class="tarjeta-titulo">Visualiza información territorial</p>
+                  <p>
+                    Explora capas, datos y documentos desde una interfaz intuitiva. Filtra, consulta
+                    y combina fuentes para generar mapas listos para descargar.
+                  </p>
+                </div>
+                <div class="tarjeta-pie">
+                  <NuxtLink class="boton-primario boton-chico" to="/consulta"
+                    >Ir al visualizador</NuxtLink
+                  >
+                </div>
+              </div>
+            </div>
+            <div class="columna-8">
+              <div class="tarjeta tarjeta-horizontal">
+                <img
+                  class="tarjeta-imagen"
+                  src="https://cdn.conahcyt.mx/sisdai/sisdai-css/documentacion/pekibb.jpg"
+                />
+                <div class="tarjeta-cuerpo">
+                  <p class="tarjeta-titulo">Analiza con Inteligencia Artificial</p>
+                  <p>
+                    Crea proyectos, define contextos y conversa con tus datos en lenguaje natural.
+                    La IA de SIGIC analiza, resume y entrega respuestas claras al instante.
+                  </p>
+                </div>
+                <div class="tarjeta-pie">
+                  <NuxtLink class="boton-primario boton-chico" to="/ia"
+                    >Iniciar análisis con IA</NuxtLink
+                  >
+                </div>
+              </div>
+            </div>
+            <div class="columna-8">
+              <div class="tarjeta tarjeta-horizontal">
+                <img
+                  class="tarjeta-imagen"
+                  src="https://cdn.conahcyt.mx/sisdai/sisdai-css/documentacion/pekibb.jpg"
+                />
+                <div class="tarjeta-cuerpo">
+                  <p class="tarjeta-titulo">Explora los recursos disponibles</p>
+                  <p>
+                    Explora capas, tablas y documentos en un solo lugar. Accede a tus archivos y a
+                    los públicos, ordénalos por tipo o fecha y encuentra rápidamente lo que buscas.
+                  </p>
+                </div>
+                <div class="tarjeta-pie">
+                  <NuxtLink class="boton-primario boton-chico" to="/catalogo"
+                    >Ir al catálogo de información</NuxtLink
+                  >
+                </div>
+              </div>
+            </div>
+            <div class="columna-8">
+              <div class="tarjeta tarjeta-horizontal">
+                <img
+                  class="tarjeta-imagen"
+                  src="https://cdn.conahcyt.mx/sisdai/sisdai-css/documentacion/pekibb.jpg"
+                />
+                <div class="tarjeta-cuerpo">
+                  <p class="tarjeta-titulo">Usa tu propia información</p>
+                  <p>
+                    Carga tus capas, tablas o documentos y publícalos en el catálogo. Georreferencia
+                    fácilmente y usa tus datos en mapas, proyectos o análisis dentro de SIGIC.
+                  </p>
+                </div>
+                <div class="tarjeta-pie">
+                  <NuxtLink class="boton-primario boton-chico" to="/catalogo"
+                    >Ir a subir archivo</NuxtLink
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="que-hacer" class="m-y-10 fondo-color-acento p-y-5">
+        <div class="contenedor ancho-lectura texto-centrado">
+          <h2>¿Qué puedes hacer en SIGIC?</h2>
+        </div>
+        <div class="contenedor ancho-fijo">
+          <div class="flex m-y-5-esc m-y-0-mov">
+            <div class="columna-8 flex-vertical-centrado">
+              <h3 class="texto-color-acento">Explorar información territorial diversa</h3>
+              <picture class="oculto-esc">
+                <img
+                  loading="lazy"
+                  class="borde-redondeado-20"
+                  src="https://cdn.conahcyt.mx/sisdai/sisdai-css/documentacion/kale-3.jpg"
+                  alt=""
+                />
+              </picture>
+              <p>
+                Consulta capas, tablas y documentos que puedes combinar fácilmente para entender
+                patrones espaciales, fenómenos sociales o contextos específicos con la información
+                científica del país.
+              </p>
+            </div>
+            <div class="columna-8">
+              <picture class="oculto-mov">
+                <img
+                  loading="lazy"
+                  class="borde-redondeado-20"
+                  src="https://cdn.conahcyt.mx/sisdai/sisdai-css/documentacion/kale-3.jpg"
+                  alt=""
+                />
+              </picture>
+            </div>
+          </div>
+          <div class="flex m-y-5-esc m-y-0-mov">
+            <div class="columna-8">
+              <picture>
+                <img
+                  loading="lazy"
+                  class="oculto-mov"
+                  src="https://cdn.conahcyt.mx/sisdai/sisdai-css/documentacion/calamidad-3.jpg"
+                  alt=""
+                />
+              </picture>
+            </div>
+            <div class="columna-8 flex-vertical-centrado">
+              <h3 class="texto-color-acento">Profundizar en temas con ayuda de la IA</h3>
+              <picture>
+                <img
+                  loading="lazy"
+                  class="oculto-esc"
+                  src="https://cdn.conahcyt.mx/sisdai/sisdai-css/documentacion/calamidad-3.jpg"
+                  alt=""
+                />
+              </picture>
+              <p>
+                Organiza la información en proyectos temáticos y analiza tus datos mediante
+                consultas en lenguaje natural. La IA del SIGIC te ayuda a interpretar contextos
+                complejos, generar síntesis, encontrar conexiones clave y preparar reportes
+                descargables en formato PDF.
+              </p>
+            </div>
+          </div>
+          <div class="flex m-y-5-esc m-y-0-mov">
+            <div class="columna-8 flex-vertical-centrado">
+              <h3 class="texto-color-acento">Aprovechar tus propios datos dentro del ecosistema</h3>
+              <picture class="oculto-esc">
+                <img
+                  loading="lazy"
+                  class="borde-redondeado-20"
+                  src="https://cdn.conahcyt.mx/sisdai/sisdai-css/documentacion/perro-3.jpg"
+                  alt=""
+                />
+              </picture>
+              <p>
+                Sube información en distintos formatos, agrégale un componente geográfico y y úsala
+                directamente en tus proyectos de IA, análisis o visualizaciones sin necesidad de
+                herramientas externas.
+              </p>
+            </div>
+            <div class="columna-8">
+              <picture class="oculto-mov">
+                <img
+                  loading="lazy"
+                  class="borde-redondeado-20"
+                  src="https://cdn.conahcyt.mx/sisdai/sisdai-css/documentacion/perro-3.jpg"
+                  alt=""
+                />
+              </picture>
+            </div>
+          </div>
+          <div class="flex m-y-5-esc m-y-0-mov">
+            <div class="columna-8">
+              <picture>
+                <img
+                  loading="lazy"
+                  class="oculto-mov"
+                  src="https://cdn.conahcyt.mx/sisdai/sisdai-css/documentacion/asha.jpg"
+                  alt=""
+                />
+              </picture>
+            </div>
+            <div class="columna-8 flex-vertical-centrado">
+              <h3 class="texto-color-acento">Tener tu información ordenada y disponible</h3>
+              <picture>
+                <img
+                  loading="lazy"
+                  class="oculto-esc"
+                  src="https://cdn.conahcyt.mx/sisdai/sisdai-css/documentacion/asha.jpg"
+                  alt=""
+                />
+              </picture>
+              <p>
+                Accede fácilmente a todos tus archivos personales o a datos públicos desde un solo
+                catálogo. Filtra por tipo, tema o fecha, y encuentra lo que necesitas con el
+                buscador integrado.
+              </p>
+            </div>
+          </div>
+          <div class="flex m-y-5-esc m-y-0-mov">
+            <div class="columna-8 flex-vertical-centrado">
+              <h3 class="texto-color-acento">
+                Compartir tus visualizaciones mediante un enlace único
+              </h3>
+              <picture class="oculto-esc">
+                <img
+                  loading="lazy"
+                  class="borde-redondeado-20"
+                  src="https://cdn.conahcyt.mx/sisdai/sisdai-css/documentacion/kale-2.jpg"
+                  alt=""
+                />
+              </picture>
+              <p>
+                ¿Quieres que tu equipo o colegas accedan exactamente a lo que estás viendo? Con un
+                enlace único, puedes compartir tus tablas, documentos o capas con todos los detalles
+                que hayas seleccionado.
+              </p>
+            </div>
+            <div class="columna-8">
+              <picture class="oculto-mov">
+                <img
+                  loading="lazy"
+                  class="borde-redondeado-20"
+                  src="https://cdn.conahcyt.mx/sisdai/sisdai-css/documentacion/kale-2.jpg"
+                  alt=""
+                />
+              </picture>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   </div>
 </template>
