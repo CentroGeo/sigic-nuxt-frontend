2235a0672c1ccc0703cc291129afa2c0fd728420
Alejandra <fasa297@gmail.com>
2025-08-28 18:23:10 -0600

    Recuperando la version previa en comentarios

--- server/api/catalogo.get.ts
+++ server/api/catalogo.get.ts
@@ -4,25 +4,29 @@ export default defineEventHandler(async (event) => {
   const api = `${config.public.geonodeApi}/resources`;
 
   let allResults: object[] = [];
+  //const query: URLSearchParams = getQuery(event);
   const query = getQuery(event);
-  console.log(query)
   const token = getHeader(event, 'token');
   const options: RequestInit = { method: 'GET' };
   if (token !== undefined) {
     options.headers = { Authorization: `Bearer ${token}` };
   }
+
+  //const dataParams = new URLSearchParams(query);
   const dataParams = new URLSearchParams();
-  const filtersDict = Object.keys(query)
+  const filtersDict = Object.keys(query);
   filtersDict.forEach((filter) => {
-    const value = query[filter]
+    const value = query[filter];
     if (typeof value === 'string') {
       dataParams.append(filter, value);
     } else if (Array.isArray(value)) {
-      value.forEach((option) => dataParams.append(filter, option))
+      value.forEach((option) => dataParams.append(filter, option));
     }
-  })
+  });
+
+  //   let endpoint = `${api}?${dataParams.toString()}`;
   let endpoint = `${api}?${dataParams.toString().replace('extent_ne=%5B-1%2C-1%2C0%2C0%5D', 'extent_ne=[-1,-1,0,0]')}`;
-  console.log(endpoint)
+  //console.log(endpoint)
 
   do {
     const response = await fetch(endpoint.replace('http:', 'https:'), options);
