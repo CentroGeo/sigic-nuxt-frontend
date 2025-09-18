4226e2dd9dfa96dbc5784c058c6098433cb07bc2
Alejandra <fasa297@gmail.com>
2025-09-18 15:09:21 -0600

    Fix: No habia actualizado la leyenda del boton para excluir capas privadas

--- utils/catalogo/index.js
+++ utils/catalogo/index.js
@@ -1,10 +1,8 @@
 export async function fetchByPk(pk) {
-  console.log('Se llamó exitosamente la función');
   const config = useRuntimeConfig();
   const { gnoxyUrl } = useGnoxyUrl();
   const api = gnoxyUrl(`${config.public.geonodeApi}/resources/${pk}`);
   const res = await fetch(api);
-  console.log('la res by pk:', res);
   const response = await res.json();
   return response.resource;
 }
