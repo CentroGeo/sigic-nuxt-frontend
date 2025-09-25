c0cd837c57b282e30398075175a5e89da08da20f
Alejandra <fasa297@gmail.com>
2025-09-25 17:33:17 -0600

    Bug: buscando por que no carga la vista de pdfs

--- components/consulta/VisualizacionDocumento.vue
+++ components/consulta/VisualizacionDocumento.vue
@@ -1,10 +1,9 @@
 <script setup>
 import { onBeforeUnmount, ref } from 'vue';
-import { resourceTypeDic } from '~/utils/consulta';
+import { fetchDoc, resourceTypeDic } from '~/utils/consulta';
 
 const storeResources = useResourcesConsultaStore();
 const storeSelected = useSelectedResources2Store();
-const { gnoxyUrl } = useGnoxyUrl();
 const resourceType = resourceTypeDic.document;
 const embedRef = ref(null);
 const selectedUuid = computed(() => storeSelected.lastVisible()?.uuid ?? null);
@@ -13,7 +12,7 @@ const selectedElement = computed(() => {
   return storeResources.findResources([selectedUuid.value], resourceType)[0] ?? null;
 });
 let resizeObserver;
-const extensionDocumento = computed(() => {
+/* const extensionDocumento = computed(() => {
   const linkCargado = selectedElement.value.links.find((link) => link.link_type === 'uploaded');
   if (linkCargado.url) {
     return linkCargado.extension;
@@ -24,8 +23,8 @@ const urlEmbebido = ref(
   extensionDocumento.value === 'pdf'
     ? selectedElement.value.embed_url
     : selectedElement.value.embed_url.replace('/embed', '/link')
-);
-/* const urlEmbebido = ref();
+); */
+const urlEmbebido = ref();
 const extensionDocumento = ref();
 const blobedUrl = ref();
 async function updateValues() {
@@ -33,23 +32,22 @@ async function updateValues() {
   extensionDocumento.value = linkCargado.extension;
   urlEmbebido.value = selectedElement.value.embed_url.replace('/embed', '/link');
   blobedUrl.value = await fetchDoc(urlEmbebido.value);
-} */
-//updateValues();
+}
+updateValues();
 watch(selectedElement, (nv) => {
   (async () => {
     // console.log('cambio el uuid');
     if (nv) {
       // limpiar antes de volver a asignar
       urlEmbebido.value = null;
-      //blobedUrl.value = null;
-      //extensionDocumento.value = null;
-      // esperar a que el DOM reaccione
+      blobedUrl.value = null;
+      extensionDocumento.value = null;
+      //esperar a que el DOM reaccione
       await nextTick();
 
-      urlEmbebido.value =
-        extensionDocumento.value === 'pdf' ? nv.embed_url : nv.embed_url.replace('/embed', '/link');
-      //console.log(urlEmbebido.value);
-      //updateValues();
+      /*       urlEmbebido.value =
+        extensionDocumento.value === 'pdf' ? nv.embed_url : nv.embed_url.replace('/embed', '/link');*/
+      updateValues();
       // esperar a que el <embed> esté en DOM
       await nextTick();
 
@@ -80,7 +78,7 @@ onBeforeUnmount(() => {
     <embed
       v-if="urlEmbebido"
       ref="embedRef"
-      :src="gnoxyUrl(urlEmbebido)"
+      :src="blobedUrl"
       :type="extensionDocumento === 'pdf' ? 'application/pdf' : 'text/plain'"
       class="documento-embebido"
     />
