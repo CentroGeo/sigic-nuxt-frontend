02dd98c1af3e8586c1143fec9165c0d601984ff8
Alejandra <fasa297@gmail.com>
2025-09-25 19:14:29 -0600

    Refactor: Se refactorizó la padina catalogo/mis-archivos

--- pages/catalogo/explorar/index.vue
+++ pages/catalogo/explorar/index.vue
@@ -1,5 +1,44 @@
 <script setup>
 import { resourceTypeDic } from '~/utils/consulta';
+const storeResources = useResourcesCatalogoStore();
+storeResources.getTotalResources(resourceTypeDic.dataLayer);
+storeResources.getTotalResources(resourceTypeDic.dataTable);
+storeResources.getTotalResources(resourceTypeDic.document);
+
+const types = ['dataLayer', 'dataTable', 'document'];
+const resourcesDict = computed(() => ({
+  dataLayer: {
+    title: 'Capas Geográficas',
+    total: storeResources.totalByType('dataLayer'),
+    latest: storeResources.latestByType('dataLayer'),
+    to: '/catalogo/explorar/capas',
+    img: '/img/thumbnail-capas.png',
+    keyword: 'capas',
+    consultaTo: '/consulta/capas',
+    consultaLabel: 'Ver Capa en el Visualizador',
+  },
+  dataTable: {
+    title: 'Datos Tabulados',
+    total: storeResources.totalByType('dataTable'),
+    latest: storeResources.latestByType('dataTable'),
+    to: '/catalogo/explorar/tablas',
+    img: '/img/thumbnail-tablas.png',
+    keyword: 'datos tabulados',
+    consultaTo: '/consulta/tablas',
+    consultaLabel: 'Ver Archivo en el Visualizador',
+  },
+  document: {
+    title: 'Documentos',
+    total: storeResources.totalByType('document'),
+    latest: storeResources.latestByType('document'),
+    to: '/catalogo/explorar/documentos',
+    img: '/img/thumbnail-docs.png',
+    keyword: 'documentos',
+    consultaTo: '/consulta/documentos',
+    consultaLabel: 'Ver Documento en el Visualizador',
+  },
+}));
+/* import { resourceTypeDic } from '~/utils/consulta';
 const storeFetched = useFetchedResources2Store();
 
 storeFetched.checkFilling(resourceTypeDic.dataLayer);
@@ -11,7 +50,7 @@ storeFetched.checkFilling(resourceTypeDic.document);
  * @param {String} type tipo de recursos a obtener el tamaño.
  * @returns {Number} número de recursos.
  */
-const obtenerLength = (type) => {
+/*const obtenerLength = (type) => {
   return computed(() => storeFetched.byResourceType(type).length || false);
 };
 
@@ -24,7 +63,7 @@ const resourcesDocumentosLength = obtenerLength(resourceTypeDic.document);
  * @param {String} type tipo de recursos a obtener más reciente.
  * @returns {Object} objeto de recursos más reciente.
  */
-const obtenerMasReciente = (type) => {
+/*const obtenerMasReciente = (type) => {
   return computed(() => storeFetched.byResourceType(type)[0] || false);
 };
 
@@ -46,7 +85,7 @@ const formatearAbstract = (resource) => {
       >${formatedAbstract}
     </p>`;
   return content;
-};
+}; */
 </script>
 
 <template>
@@ -60,6 +99,23 @@ const formatearAbstract = (resource) => {
         <main id="principal" class="contenedor m-b-10 p-t-3">
           <h2>Explora</h2>
           <div class="flex">
+            <div v-for="type in types" :key="`explora-${type}`" class="columna-5">
+              <nuxt-link class="tarjeta tarjeta-hipervinculo-interno" :to="resourcesDict[type].to">
+                <img class="tarjeta-imagen" :src="resourcesDict[type].img" alt="" />
+                <div class="tarjeta-cuerpo">
+                  <p class="tarjeta-titulo">{{ resourcesDict[type].title }}</p>
+                  <p class="tarjeta-etiqueta">
+                    {{
+                      !resourcesDict[type].total
+                        ? '...cargando'
+                        : resourcesDict[type].total + ' capas'
+                    }}
+                  </p>
+                </div>
+              </nuxt-link>
+            </div>
+          </div>
+          <!-- <div class="flex">
             <div class="columna-5">
               <nuxt-link class="tarjeta tarjeta-hipervinculo-interno" to="/catalogo/explorar/capas">
                 <img class="tarjeta-imagen" src="/img/thumbnail-capas.png" alt="" />
@@ -107,10 +163,45 @@ const formatearAbstract = (resource) => {
                 </div>
               </nuxt-link>
             </div>
-          </div>
+          </div> -->
 
           <h2>Lo más reciente</h2>
           <div class="flex">
+            <div v-for="type in types" :key="`reciente-${type}`" class="columna-5">
+              <div class="tarjeta">
+                <img
+                  v-if="resourcesDict[type].latest"
+                  class="tarjeta-imagen"
+                  :src="resourcesDict[type].latest.thumbnail_url"
+                  alt=""
+                />
+                <div class="tarjeta-cuerpo">
+                  <p class="tarjeta-etiqueta">{{ resourcesDict[type].title }}</p>
+                  <p v-if="!resourcesDict[type].latest">...cargando</p>
+                  <p v-if="resourcesDict[type].latest" class="tarjeta-titulo">
+                    {{ resourcesDict[type].latest.title }}
+                  </p>
+                  <span v-if="resourcesDict[type].latest">
+                    <div>
+                      <p class="abstract">
+                        {{ resourcesDict[type].latest.raw_abstract }}
+                      </p>
+                    </div>
+                  </span>
+                </div>
+                <div class="tarjeta-pie">
+                  <nuxt-link
+                    class="boton boton-primario boton-chico"
+                    aria-label="Ver capa en visualizador"
+                    :to="resourcesDict[type].consultaTo"
+                  >
+                    {{ resourcesDict[type].consultaLabel }}
+                  </nuxt-link>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- <div class="flex">
             <div class="columna-5">
               <div class="tarjeta">
                 <img class="tarjeta-imagen" :src="capaMasReciente.thumbnail_url" alt="" />
@@ -121,8 +212,8 @@ const formatearAbstract = (resource) => {
                     {{ capaMasReciente.title }}
                   </p>
                   <span v-if="capaMasReciente">
-                    <!-- eslint-disable-next-line vue/no-v-html -->
-                    <div v-html="formatearAbstract(capaMasReciente)"></div>
+                     eslint-disable-next-line vue/no-v-html -->
+          <!--<div v-html="formatearAbstract(capaMasReciente)"></div>
                   </span>
                 </div>
                 <div class="tarjeta-pie">
@@ -146,8 +237,8 @@ const formatearAbstract = (resource) => {
                     {{ tablaMasReciente.title }}
                   </p>
                   <span v-if="tablaMasReciente">
-                    <!-- eslint-disable-next-line vue/no-v-html -->
-                    <div v-html="formatearAbstract(tablaMasReciente)"></div>
+                     eslint-disable-next-line vue/no-v-html -->
+          <!-- <div v-html="formatearAbstract(tablaMasReciente)"></div>
                   </span>
                 </div>
                 <div class="tarjeta-pie">
@@ -171,8 +262,8 @@ const formatearAbstract = (resource) => {
                     {{ documentoMasReciente.title }}
                   </p>
                   <span v-if="documentoMasReciente">
-                    <!-- eslint-disable-next-line vue/no-v-html -->
-                    <span v-html="formatearAbstract(documentoMasReciente)"></span>
+                     eslint-disable-next-line vue/no-v-html -->
+          <!-- <span v-html="formatearAbstract(documentoMasReciente)"></span>
                   </span>
                 </div>
                 <div class="tarjeta-pie">
@@ -186,9 +277,17 @@ const formatearAbstract = (resource) => {
                 </div>
               </div>
             </div>
-          </div>
+          </div> -->
         </main>
       </template>
     </UiLayoutPaneles>
   </div>
 </template>
+<style>
+.abstract {
+  text-overflow: ellipsis;
+  overflow: hidden;
+  height: 1.2em;
+  white-space: nowrap;
+}
+</style>
---
+++ stores/resourcesCatalogo.js
@@ -0,0 +1,97 @@
+import { defineStore } from 'pinia';
+import { buildUrl, resourceTypeDic, resourceTypeGeonode } from '~/utils/consulta';
+
+export const useResourcesCatalogoStore = defineStore('resourcesCatalogo', () => {
+  const config = useRuntimeConfig();
+  const storeConsulta = useConsultaStore();
+  /**
+   * Almacenamiento reactivo de los recursos seleccionados.
+   */
+  const totals = reactive({
+    [resourceTypeDic.dataLayer]: 0,
+    [resourceTypeDic.dataTable]: 0,
+    [resourceTypeDic.document]: 0,
+  });
+  const resources = reactive({
+    [resourceTypeDic.dataLayer]: [],
+    [resourceTypeDic.dataTable]: [],
+    [resourceTypeDic.document]: [],
+  });
+
+  const latestResources = reactive({
+    [resourceTypeDic.dataLayer]: undefined,
+    [resourceTypeDic.dataTable]: undefined,
+    [resourceTypeDic.document]: undefined,
+  });
+
+  return {
+    isLoading: ref(false),
+    totals,
+    resources,
+    latestResources,
+
+    resourcesByType(resourceType = storeConsulta.resourceType) {
+      return resources[resourceType];
+    },
+
+    totalByType(resourceType = storeConsulta.resourceType) {
+      return totals[resourceType];
+    },
+
+    latestByType(resourceType = storeConsulta.resourceType) {
+      return latestResources[resourceType];
+    },
+
+    resetByType(resourceType = storeConsulta.resourceType) {
+      totals[resourceType] = 0;
+      resources[resourceType] = [];
+    },
+
+    async getTotalResources(resourceType = storeConsulta.resourceType) {
+      const { gnoxyFetch } = useGnoxyUrl();
+      this.isLoading = true;
+      const queryParams = {
+        custom: 'true',
+        'filter{resource_type}': resourceTypeGeonode[resourceType],
+        // TODO: Cambiar este valor por un 1
+        page_size: 100,
+      };
+      if (resourceType === 'dataLayer') {
+        queryParams['extent_ne'] = '[-1,-1,0,0]';
+      }
+      if (resourceType === 'dataTable') {
+        //queryParams['filter{subtype.in}'] = ['vector', 'remote'];
+        queryParams['filter{subtype.in}'] = 'vector';
+      }
+      /*  if (resourceType === 'document') {
+        queryParams['file_extension'] = ['pdf', 'txt'];
+      } */
+
+      const url = buildUrl(`${config.public.geonodeApi}/resources`, queryParams);
+      const request = await gnoxyFetch(url.toString());
+      const res = await request.json();
+      totals[resourceType] = res.total;
+      latestResources[resourceType] = res.resources[0];
+      this.isLoading = false;
+    },
+
+    /**
+     * Devuelve un recursos que coincida con un uuid.
+     * @param {String} uuid del catalogo a buscar.
+     * @param {String} resourceType tipo de resursos a consultar.
+     * @returns {Object} ojeto de recursos de geonode.
+     */
+    findResource(uuidToFind, resourceType = storeConsulta.resourceType) {
+      return resources[resourceType].find(({ uuid }) => uuid === uuidToFind);
+    },
+    /**
+     * Devuelve una lista de recursos que coincidan con una lista de uuids.
+     * @param {Array<String>} uuids del catalogo a buscar.
+     * @param {String} resourceType tipo de resursos a consultar.
+     * @returns {Array<Object>} lista de ojetos de recursos de geonode.
+     */
+    findResources(uuidsToFind, resourceType = storeConsulta.resourceType) {
+      return resources[resourceType].filter(({ uuid }) => uuidsToFind.includes(uuid));
+    },
+  };
+});
