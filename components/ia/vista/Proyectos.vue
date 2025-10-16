6b6069fe04a21a4bcf9ba34360cc4990bc1dff47
Jose Luis <juc@centrogeo.edu.mx>
2025-10-16 14:59:35 -0600

    Agregados botones para eliminar proyecto y contexto

--- components/ia/vista/Proyectos.vue
+++ components/ia/vista/Proyectos.vue
@@ -136,19 +136,21 @@ async function openResourceView(resource) {
                 <span class="pictograma-privado" aria-hidden="true" />
               </p>
             </div>
-            <nuxt-link
-              class="boton boton-secundario boton-chico"
-              aria-label="Configurar proyecto"
-              :to="`/ia/proyecto/${proyecto.id}`"
-            >
-              Configurar proyecto
-            </nuxt-link>
-            <!-- <button
-              class="boton boton-primario boton-chico"
-              @click="storeIA.eliminarProyecto(proyecto.id)"
-            >
-              Eliminar proyecto
-            </button> -->
+            <div class="flex">
+              <nuxt-link
+                class="boton boton-secundario boton-chico"
+                aria-label="Configurar proyecto"
+                :to="`/ia/proyecto/${proyecto.id}`"
+              >
+                Configurar proyecto
+              </nuxt-link>
+              <button
+                class="boton boton-secundario boton-chico"
+                @click="storeIA.eliminarProyecto(proyecto.id)"
+              >
+                Eliminar proyecto
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -209,14 +211,14 @@ async function openResourceView(resource) {
                       Editar contexto
                       <span class="pictograma-editar" aria-hidden="true" />
                     </nuxt-link>
-                    <!-- <nuxt-link
+                    <nuxt-link
                       class="boton-secundario boton-chico"
                       type="button"
                       @click="storeIA.eliminarContexto(contexto.id)"
                     >
                       Eliminar contexto
                       <span class="pictograma-eliminar" aria-hidden="true" />
-                    </nuxt-link> -->
+                    </nuxt-link>
                   </div>
                 </div>
               </div>
