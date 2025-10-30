39d4f580fac87e78782d1326c3d435df16b1e1ee
Niji <mathereall@gmail.com>
2025-10-30 01:47:36 -0600

    fix: update nuxt.config.ts to correctly set basePath and baseURL for development

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from 'nuxt/config';
 
 const isDev = process.env.NODE_ENV !== 'production';
 const appBasePath = process.env.NUXT_PUBLIC_APP_BASE_PATH || '/';
-// const basePath = appBasePath.replace(/\/+$/, '');
+const basePath = appBasePath.replace(/\/+$/, '');
 const origin = process.env.NUXT_PUBLIC_BASE_URL;
 const authBaseUrl = process.env.NUXT_PUBLIC_AUTH_BASE_URL;
 const originEnvKey = isDev ? undefined : 'NUXT_AUTH_ORIGIN';
@@ -74,6 +74,7 @@ export default defineNuxtConfig({
     isEnabled: true,
     baseURL: authBaseUrl,
     originEnvKey: originEnvKey,
+    basePath: basePath,
     origin: origin,
     globalAppMiddleware: false,
     provider: {
@@ -97,7 +98,8 @@ export default defineNuxtConfig({
 
     // Variables públicas (disponibles también en el cliente)
     public: {
-      baseURL: process.env.NUXT_PUBLIC_BASE_URL || 'https://geonode.dev.geoint.mx',
+      baseURL: process.env.NUXT_PUBLIC_BASE_URL || 'http://localhost:3000',
+      basePath: basePath,
       geonodeUrl: process.env.NUXT_PUBLIC_GEONODE_URL || 'https://geonode.dev.geoint.mx',
       geonodeApi: process.env.NUXT_PUBLIC_GEONODE_API || 'https://geonode.dev.geoint.mx/api/v2',
       geoserverUrl:
