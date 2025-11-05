9088e720a2d568d34383d868a80afb5208b650c8
Niji <mathereall@gmail.com>
2025-11-05 03:18:13 -0600

    fix: update Chats.vue and nuxt.config.ts to rename iAModel to ollamaModel

--- components/ia/vista/Chats.vue
+++ components/ia/vista/Chats.vue
@@ -177,7 +177,7 @@ const submitMensaje = async () => {
     //context_id: 9,
     // model: 'deepseek-r1',
     // model: 'qwen2:1.5b',
-    model: config.public.iAModel,
+    model: config.public.ollamaModel,
     //model: "llama3.1",
     messages: [
       {
