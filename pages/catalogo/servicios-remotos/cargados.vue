3620828af7025d2710d2024d13666914f22ac0ac
Alejandra <fasa297@gmail.com>
2025-12-05 13:54:59 -0600

    Agregando codigo de respaldo

--- pages/catalogo/servicios-remotos/cargados.vue
+++ pages/catalogo/servicios-remotos/cargados.vue
@@ -8,35 +8,29 @@ definePageMeta({
 });
 
 const storeCatalogo = useCatalogoStore();
-const { gnoxyFetch } = useGnoxyUrl();
 
-//const { data } = useAuth();
-//const token = data.value?.accessToken;
+const { data } = useAuth();
+const token = data.value?.accessToken;
 // console.log(token);
-//const headers = ref({ Authorization: `Bearer ${token}`, 'Content-Type': 'application/json' });
+const headers = ref({ Authorization: `Bearer ${token}`, 'Content-Type': 'application/json' });
 const configEnv = useRuntimeConfig();
 const baseUrl = configEnv.public.geonodeApi;
-//const harvesters = ref([]);
-//const resources = ref([]);
-const paginaActual = ref(0);
-const tamanioPagina = 10;
-const totalHarvesters = ref(0);
-const totalPags = computed(() => Math.ceil(totalHarvesters.value / tamanioPagina));
+const harvesters = ref([]);
+const resources = ref([]);
 const remoteHaverstersResources = ref([]);
-//const harvestableResources = ref([]);
-//const cargando = ref(false);
+const harvestableResources = ref([]);
+const cargando = ref(false);
 
 const irARutaQuery = (v, destino) => {
-  //cargando.value = true;
+  cargando.value = true;
   if (destino !== 'pendientes') {
     navigateTo({
       path: `/catalogo/servicios-remotos/${v.id}`,
       query: {
         id: v.id,
         title: v.title,
-        total: v.exported_resources + v.to_attend_resources,
-        /*         unique_identifier: v.unique_identifier,
-        remote_resource_type: v.remote_resource_type, */
+        unique_identifier: v.unique_identifier,
+        remote_resource_type: v.remote_resource_type,
       },
     });
   } else {
@@ -45,51 +39,78 @@ const irARutaQuery = (v, destino) => {
       query: {
         id: v.id,
         title: v.title,
-        /*         unique_identifier: v.unique_identifier,
-        remote_resource_type: v.remote_resource_type, */
+        unique_identifier: v.unique_identifier,
+        remote_resource_type: v.remote_resource_type,
       },
     });
   }
 };
 
-async function fetchHarvestersInfo() {
-  try {
-    const requestHarvesters = await gnoxyFetch(
-      `${baseUrl}/harvesters/?page_size=${tamanioPagina}&page=${paginaActual.value + 1}`
-    );
-    const responseHarvesters = await requestHarvesters.json();
-    totalHarvesters.value = responseHarvesters.total;
-    const harvesters = responseHarvesters.harvesters;
+try {
+  // petición a los recursos consechados de services remotos
+  const responseHaversters = await $fetch(`${baseUrl}/harvesters`, {
+    method: 'GET',
+    headers: headers.value,
+  });
+  // console.log('responseHaversters', responseHaversters);
+  harvesters.value = responseHaversters.harvesters;
+  const responseResources = await $fetch(`${baseUrl}/resources?filter{resource_type}=service`, {
+    method: 'GET',
+    headers: headers.value,
+  });
+  // console.log('responseResources', responseResources);
+  resources.value = responseResources.resources;
 
-    harvesters.forEach(async (h) => {
-      const harvestableResourcesUrl = h.links.harvestable_resources;
-      const resA = await gnoxyFetch(`${harvestableResourcesUrl}/?page_size=1`);
-      const dataA = await resA.json();
-      const totalResources = dataA.total;
+  // llenamos con los recursos remotos
+  resources.value.forEach((d) =>
+    harvesters.value.forEach(async (dd) => {
+      // el identificador del owner es un número entero
+      if (d.owner.pk === dd.default_owner) {
+        const exportedResources = ref(0);
+        harvestableResources.value = [];
+        const totalResources = ref(0);
+        const url = `${baseUrl}/harvesters/${dd.id}/harvestable-resources`;
 
-      const res2 = await gnoxyFetch(`${harvestableResourcesUrl}/?page_size=${totalResources}`);
-      const dataB = await res2.json();
-      const harvestableResources = dataB.harvestable_resources;
-      const exportedResources = harvestableResources.filter((j) => j.should_be_harvested === true);
-      remoteHaverstersResources.value.push({
-        id: h.id,
-        title: h.name,
-        exported_resources: exportedResources.length,
-        to_attend_resources: totalResources - exportedResources.length,
-        remote_url: h.remote_url,
-      });
-    });
-  } catch (err) {
-    console.warn('Error en el streaming: ' + err);
-  }
+        // obtenemos total de recursos
+        const res = await $fetch(url, {
+          method: 'GET',
+          headers: { Authorization: `Bearer ${token}`, 'Content-Type': 'application/json' },
+        });
+        totalResources.value = res.total;
+        // obtenemos todos los recursos cosechados
+        const response = await $fetch(`${url}/?page_size=${totalResources.value}`, {
+          method: 'GET',
+          headers: { Authorization: `Bearer ${token}`, 'Content-Type': 'application/json' },
+        });
+        harvestableResources.value = response.harvestable_resources;
+
+        // filtramos por los remotos ya están importados a sigic junto con tipo de recurso
+        exportedResources.value = harvestableResources.value.filter(
+          (j) => j.should_be_harvested === true && j.remote_resource_type
+        );
+
+        // push al objeto final
+        remoteHaverstersResources.value.push({
+          id: dd.id,
+          title: d.title,
+          exported_resources: exportedResources.value.length,
+          to_attend_resources: totalResources.value - exportedResources.value.length,
+          remote_url: dd.remote_url,
+
+          unique_identifier: exportedResources.value.map((e) => {
+            return e.unique_identifier;
+          }),
+          remote_resource_type: exportedResources.value.map((e) => {
+            return e.remote_resource_type;
+          }),
+        });
+        // console.log('remoteHaverstersResources.value', remoteHaverstersResources.value);
+      }
+    })
+  );
+} catch (err) {
+  console.warn('Error en el streaming: ' + err);
 }
-
-fetchHarvestersInfo();
-
-watch(paginaActual, () => {
-  remoteHaverstersResources.value = [];
-  fetchHarvestersInfo();
-});
 </script>
 
 <template>
@@ -98,12 +119,11 @@ watch(paginaActual, () => {
       <CatalogoListaMenuLateral />
     </template>
     <template #visualizador>
-      <!-- <main
+      <main
         v-if="remoteHaverstersResources.length !== 0 && !cargando"
         id="principal"
         class="contenedor m-b-10"
-      > -->
-      <main v-if="remoteHaverstersResources.length" id="principal" class="contenedor m-b-10">
+      >
         <h2>Carga catálogos externos</h2>
         <div class="ancho-fijo">
           <h3>Recursos cargados</h3>
@@ -111,65 +131,56 @@ watch(paginaActual, () => {
             class="texto-color-alerta fondo-color-alerta borde borde-color-alerta p-2 borde-redondeado-8"
           >
             Los catálogos externos tienen funciones limitadas. Algunas descargas o consultas pueden
-            no estar disponibles. <br />
-            <br />
-            Los recursos importados requieren que
-            <span style="font-weight: bold">completes sus metadatos</span> antes de poder
-            visualizarlos.
+            no estar disponibles
           </p>
-          <!--           <form> -->
-          <table>
-            <thead>
-              <tr>
-                <th>Nombre de servicio externo</th>
-                <th>Recursos importados</th>
-                <th>Recursos pendientes</th>
-                <th>URL</th>
-                <th>Tipo</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="value in remoteHaverstersResources" :key="value.id">
-                <td>{{ value.title }}</td>
-                <td>
-                  <nuxt-link @click="irARutaQuery(value, '')">
-                    {{ value.exported_resources }}
-                  </nuxt-link>
-                </td>
-                <td>
-                  <nuxt-link @click="irARutaQuery(value, 'pendientes')">
-                    {{ value.to_attend_resources }}
-                  </nuxt-link>
-                </td>
-                <td>
-                  <a :href="value.remote_url" target="_blank" rel="noopener noreferrer">
-                    {{ value.remote_url }}
-                  </a>
-                </td>
-                <td>Servcio de Mapas</td>
-              </tr>
-            </tbody>
-          </table>
-          <UiPaginador
-            :pagina-parent="paginaActual"
-            :total-paginas="totalPags"
-            @cambio="paginaActual = $event"
-          />
-          <div class="flex flex-contenido-inicio m-t-3">
-            <nuxt-link
-              class="boton boton-primario"
-              aria-label="Ir a catálogo externo en mis archivos"
-              to="/catalogo/mis-archivos/metadatos-pendientes"
-              >Editar Metadatos
-            </nuxt-link>
-            <nuxt-link
-              class="boton boton-secundario"
-              aria-label="Agregar servicio remoto"
-              to="/catalogo/servicios-remotos/agregar"
-              >Agregar Catálogo Externo
-            </nuxt-link>
-          </div>
-          <!--           </form> -->
+          <form>
+            <table>
+              <thead>
+                <tr>
+                  <th>Nombre de servicio externo</th>
+                  <th>Recursos importados</th>
+                  <th>Recursos pendientes</th>
+                  <th>URL</th>
+                  <th>Tipo</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="value in remoteHaverstersResources" :key="value.id">
+                  <td>{{ value.title }}</td>
+                  <td>
+                    <nuxt-link @click="irARutaQuery(value, '')">
+                      {{ value.exported_resources }}
+                    </nuxt-link>
+                  </td>
+                  <td>
+                    <nuxt-link @click="irARutaQuery(value, 'pendientes')">
+                      {{ value.to_attend_resources }}
+                    </nuxt-link>
+                  </td>
+                  <td>
+                    <a :href="value.remote_url" target="_blank" rel="noopener noreferrer">
+                      {{ value.remote_url }}
+                    </a>
+                  </td>
+                  <td>Servcio de Mapas</td>
+                </tr>
+              </tbody>
+            </table>
+            <div class="flex flex-contenido-inicio m-t-3">
+              <nuxt-link
+                class="boton boton-primario"
+                aria-label="Ir a catálogo externo en mis archivos"
+                to="/catalogo/mis-archivos"
+                >Ver catálogo Externo en Mis Archivos
+              </nuxt-link>
+              <nuxt-link
+                class="boton boton-secundario"
+                aria-label="Agregar servicio remoto"
+                to="/catalogo/servicios-remotos/agregar"
+                >Agregar servicio remoto
+              </nuxt-link>
+            </div>
+          </form>
         </div>
       </main>
       <main v-else>
@@ -186,7 +197,7 @@ watch(paginaActual, () => {
           <div class="columna-4"></div>
         </div>
       </main>
-      <!-- <main v-if="cargando">
+      <main v-if="cargando">
         <div class="flex">
           <div class="columna-4"></div>
           <div class="columna-8">
@@ -199,7 +210,7 @@ watch(paginaActual, () => {
           </div>
           <div class="columna-4"></div>
         </div>
-      </main> -->
+      </main>
     </template>
   </UiLayoutPaneles>
 </template>
