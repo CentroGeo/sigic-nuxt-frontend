b394345fa0adce2e3ca75dbf00cae847b4956201
Alejandra <fasa297@gmail.com>
2025-12-05 12:28:48 -0600

    Re-implementando la vista de recursos importados

--- pages/catalogo/servicios-remotos/index.vue
+++ pages/catalogo/servicios-remotos/index.vue
@@ -8,28 +8,6 @@ definePageMeta({
 
 const storeCatalogo = useCatalogoStore();
 
-/* try {
-  const { data } = useAuth();
-  const token = data.value?.accessToken;
-  // console.log(token);
-  const response = await $fetch('https://geonode.dev.geoint.mx/api/v2/harvesters/', {
-    method: 'GET',
-    headers: { Authorization: `Bearer ${token}`, 'Content-Type': 'application/json' },
-  });
-  // console.log('response', response);
-  const harvesters = response.harvesters;
-  if (harvesters.length === 0) {
-    navigateTo({
-      path: '/catalogo/servicios-remotos/agregar',
-    });
-  } else {
-    navigateTo({
-      path: '/catalogo/servicios-remotos/cargados',
-    });
-  }
-} catch (err) {
-  console.warn('Error en el streaming: ' + err);
-} */
 navigateTo({
   path: '/catalogo/servicios-remotos/agregar',
 });
