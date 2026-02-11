1fa46af1c79b45ba2ca0bba7537b82657a38a576
Alejandra <fasa297@gmail.com>
2026-02-11 12:14:52 -0600

    Ajustando manejo de errores y reintentos en petición de recursos en store de consulta pt2

--- stores/resourcesConsulta.js
+++ stores/resourcesConsulta.js
@@ -132,6 +132,7 @@ export const useResourcesConsultaStore = defineStore('resourcesConsulta', () => 
           // Agregamos los estilos
           if (resourceData.resource_type === 'dataset') {
             const { defaultStyle, styleList } = await getSLDs(resourceData);
+            console.log(defaultStyle);
             resourceData.default_style = defaultStyle;
             resourceData.styles = styleList;
           }
