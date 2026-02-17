89f5fcf16fbe12ee82b2231beb433968b33c840d
Alejandra <fasa297@gmail.com>
2026-02-17 17:02:35 -0600

    Comenzando a implementar el cambio de contraseña

--- pages/mi-cuenta/seguridad.vue
+++ pages/mi-cuenta/seguridad.vue
@@ -1,8 +1,38 @@
 <script setup>
-import SisdaiCampoBase from '@centrogeomx/sisdai-componentes/src/componentes/campo-base/SisdaiCampoBase.vue';
+import { wait } from '@/utils/consulta';
 
 definePageMeta({
   middleware: 'auth',
+});
+const config = useRuntimeConfig();
+const idps = JSON.parse(config.public.keycloakIDPs);
+const idpsInfo = ref(null);
+const isLoading = ref(true);
+
+async function buildIDPsInfo() {
+  idpsInfo.value = idps.map((d) => {
+    return {
+      alias: d.alias.trim(),
+      idp_value: d.idp_value,
+      status: d.alias.trim() === 'Google' ? 'Vinculada' : 'No Vinculada',
+    };
+  });
+  await wait(3000);
+  isLoading.value = false;
+}
+
+function updatePassword() {
+  const url = `${config.public.keycloakIssuer}/protocol/openid-connect/auth?client_id=${config.public.keycloakClientId}&redirect_uri=${encodeURIComponent('https://sigic.dev.geoint.mx/')}&response_type=code&scope=openid&kc_action=UPDATE_PASSWORD`;
+  navigateTo(url, { external: true });
+}
+
+function linkSocialAccount(idp) {
+  const url = `${config.public.keycloakIssuer}/protocol/openid-connect/auth?client_id=${config.public.keycloakClientId}&redirect_uri=${encodeURIComponent('https://sigic.dev.geoint.mx/')}&response_type=code&scope=openid&kc_action=idp_link:${idp}`;
+  navigateTo(url, { external: true });
+}
+
+onMounted(() => {
+  buildIDPsInfo();
 });
 </script>
 <template>
@@ -10,32 +40,65 @@ definePageMeta({
     <h2>Seguridad</h2>
     <div class="p-x-7 p-b-5">
       <h3>Cambiar contraseña</h3>
-      <p>
+      <p class="m-0">
         Elige una nueva contraseña para tu cuenta. Es posible que se cierre tu sesión en otros
         dispositivos.
       </p>
-      <p>
+      <p class="m-0">
         Usa al menos 8 caracteres, incluyendo una letra mayúscula, una letra minúscula, un número y
         un símbolo.
       </p>
-      <SisdaiCampoBase
-        class="m-y-2"
-        etiqueta="Contraseña actual"
-        ejemplo="Introduce tu contraseña actual"
-        tipo="password"
-        :es_obligatorio="false"
-        :es_etiqueta_visible="true"
-      />
-      <SisdaiCampoBase
-        class="m-y-2"
-        etiqueta="Contraseña nueva"
-        ejemplo="Introduce la contraseña nueva"
-        tipo="password"
-        :es_obligatorio="false"
-        :es_etiqueta_visible="true"
-      />
-      <button class="boton-primario boton-chico">
-        Guardar cambios <span class="pictograma-guardar"></span>
+
+      <div class="m-y-2">
+        <label for="current-password">Contraseña actual</label>
+        <input
+          id="current-password"
+          type="password"
+          name="current-password"
+          minlength="8"
+          placeholder="Introduce la contaseña actual"
+          :disabled="true"
+        />
+      </div>
+
+      <div class="m-y-2">
+        <label for="new-password">Contraseña nueva</label>
+        <input
+          id="new-password"
+          type="password"
+          name="current-password"
+          minlength="8"
+          placeholder="Introduce la contaseña nueva"
+          :disabled="true"
+        />
+      </div>
+      <!--       <ClientOnly>
+        <SisdaiCampoBase
+          class="m-y-2"
+          etiqueta="Contraseña actual"
+          ejemplo="Introduce tu contraseña actual"
+          tipo="password"
+          :es_obligatorio="false"
+          :es_etiqueta_visible="true"
+        />
+      </ClientOnly>
+      <ClientOnly>
+        <SisdaiCampoBase
+          class="m-y-2"
+          etiqueta="Contraseña nueva"
+          ejemplo="Introduce la contraseña nueva"
+          tipo="password"
+          :es_obligatorio="false"
+          :es_etiqueta_visible="true"
+        />
+      </ClientOnly> -->
+
+      <!-- <button class="boton-primario boton-chico m-y-2" >
+        Guardar Contraseña <span class="pictograma-guardar"></span>
+      </button> -->
+
+      <button class="boton-primario boton-chico m-y-2" @click="updatePassword">
+        Cambiar Contraseña
       </button>
 
       <h4>Vincular cuentas</h4>
@@ -43,72 +106,53 @@ definePageMeta({
         Puedes conectar tus cuentas para facilitar el inicio de sesión y mantener sincronizada tu
         información profesional.
       </p>
-      <div class="tarjeta m-y-3">
-        <div class="tarjeta-cuerpo">
-          <div class="flex flex-contenido-separado">
-            <div class="flex-vertical-centrado">Google</div>
-            <div class="flex">
-              <span
-                class="flex flex-vertical-centrado texto-color-confirmacion fondo-color-confirmacion borde borde-color-confirmacion borde-redondeado-4 p-x-1"
-                >Vinculada</span
-              >
-              <button class="boton-chico boton-secundario">Desvincular</button>
+      <!-- Spinner-->
+      <div v-if="isLoading" class="flex flex-contenido-centrado m-y-5">
+        <img class="color-invertir" src="/img/loader.gif" alt="...Cargando" height="120px" />
+      </div>
+      <div v-else>
+        <!-- Las tarjetas de cuentas vinculadas -->
+        <div v-for="broker in idpsInfo" :key="broker.idp_value" class="tarjeta m-y-3">
+          <div class="tarjeta-cuerpo">
+            <div class="flex flex-contenido-separado">
+              <div class="flex-vertical-centrado">{{ broker.alias }}</div>
+              <div class="flex">
+                <span
+                  v-if="broker.status === 'Vinculada'"
+                  class="flex flex-vertical-centrado texto-color-confirmacion fondo-color-confirmacion borde borde-color-confirmacion borde-redondeado-4 p-x-1"
+                  >Vinculada</span
+                >
+                <span
+                  v-if="broker.status === 'No Vinculada'"
+                  class="flex flex-vertical-centrado texto-color-alerta fondo-color-alerta borde borde-color-alerta borde-redondeado-4 p-x-1"
+                  >No vinculada</span
+                >
+                <button v-if="broker.status === 'Vinculada'" class="boton-chico boton-secundario">
+                  Desvincular
+                </button>
+                <button
+                  v-if="broker.status === 'No Vinculada'"
+                  class="boton-chico boton-primario"
+                  @click="linkSocialAccount(broker.idp_value)"
+                >
+                  Vincular
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div class="tarjeta m-y-3">
-        <div class="tarjeta-cuerpo">
-          <div class="flex flex-contenido-separado">
-            <div class="flex-vertical-centrado">Rizoma</div>
-            <div class="flex">
-              <span
-                class="flex flex-vertical-centrado texto-color-alerta fondo-color-alerta borde borde-color-alerta borde-redondeado-4 p-x-1"
-                >No vinculada</span
-              >
-              <button class="boton-chico boton-primario">Conectar</button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="tarjeta m-y-3">
-        <div class="tarjeta-cuerpo">
-          <div class="flex flex-contenido-separado">
-            <div class="flex-vertical-centrado">ORCID</div>
-            <div class="flex">
-              <span
-                class="flex flex-vertical-centrado texto-color-alerta fondo-color-alerta borde borde-color-alerta borde-redondeado-4 p-x-1"
-                >No vinculada</span
-              >
-              <button class="boton-chico boton-primario">Conectar</button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="tarjeta m-y-3">
-        <div class="tarjeta-cuerpo">
-          <div class="flex flex-contenido-separado">
-            <div class="flex-vertical-centrado">GitHub</div>
-            <div class="flex">
-              <span
-                class="flex flex-vertical-centrado texto-color-alerta fondo-color-alerta borde borde-color-alerta borde-redondeado-4 p-x-1"
-                >No vinculada</span
-              >
-              <button class="boton-chico boton-primario">Conectar</button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="tarjeta m-y-3 fondo-color-informacion texto-color-informacion">
-        <div class="tarjeta-cuerpo">
-          <p class="nota-titulo texto-color-informacion">
-            <span class="pictograma-informacion" /> Información sobre las cuentas vinculadas
-          </p>
+        <!-- Cuadro de información -->
+        <div class="tarjeta m-y-3 fondo-color-informacion texto-color-informacion">
+          <div class="tarjeta-cuerpo">
+            <p class="nota-titulo texto-color-informacion">
+              <span class="pictograma-informacion" /> Información sobre las cuentas vinculadas
+            </p>
 
-          <p class="nota texto-color-informacion">
-            Si desvinculas alguna cuenta, podrás seguir accediendo con tu usuario y contraseña del
-            SIGIC, sin embargo, dejarás de poder iniciar sesión con esa plataforma vinculada.
-          </p>
+            <p class="nota texto-color-informacion">
+              Si desvinculas alguna cuenta, podrás seguir accediendo con tu usuario y contraseña del
+              SIGIC, sin embargo, dejarás de poder iniciar sesión con esa plataforma vinculada.
+            </p>
+          </div>
         </div>
       </div>
     </div>
