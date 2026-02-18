199ccbd7312a7d1a1312d9751623babc62edeb28
César Benjamín <mathereall@gmail.com>
2026-02-18 15:37:06 -0600

    fix: append trailing slashes to baseURL and basePath in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 import { defineNuxtConfig } from 'nuxt/config';
 
 const isDev = process.env.NODE_ENV !== 'production';
-const appBasePath = process.env.NUXT_APP_BASE_URL || '/';
+const appBasePath = process.env.NUXT_APP_BASE_URL || '';
 const authBaseUrl = process.env.NUXT_PUBLIC_AUTH_BASE_URL;
 const originEnvKey = isDev ? undefined : 'NUXT_AUTH_ORIGIN';
 
