fd75657b141c742d7973923e178f7e30f55c0e5b
Jesús Daniel Gómez Hernández <psp.dgomez@centrogeo.edu.mx>
2026-02-23 11:43:58 -0600

    un commit para revisar develop

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const { status } = useAuth();
-
+  console.log(status);
   // Si no está autenticado y no está cargando, redirigir al inicio
   if (status.value !== 'authenticated') {
     return navigateTo('/');
