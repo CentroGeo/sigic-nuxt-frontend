63ebe51abbbe47d9baf968db96e2ea1a9143e931
Fernando Valle <fersoriano@ciencias.una.mx>
2026-06-09 14:16:51 -0600

    Revert "fix: deshabilitar SSR para evitar recursión de nuxt-auth en despliegue con proxy externo"
    This reverts commit 917f2295422711270a276c3baca334da8005d7c8.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,8 +12,6 @@ const metaDescription =
   'Sistema Integral de Gestión de Información Científica. Integra, visualiza y aprovecha el conocimiento científico de México.';
 
 export default defineNuxtConfig({
-  ssr: false,
-
   app: {
     baseURL: appBasePath,
     head: {
